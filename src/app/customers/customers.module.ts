562237596bb4885c699a17350aa135d540a96064
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-09-28 19:15:40 -0300

    create moneySign pipe

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   declarations: [
     CustomersComponent,
     CustomersListComponent,
