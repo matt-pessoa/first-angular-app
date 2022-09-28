1221894e25df2f520ca997302e8025dfcd5c4537
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-09-28 16:33:41 -0300

    update index.html metadata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   imports: [BrowserModule, CustomersModule],
   declarations: [AppComponent],
