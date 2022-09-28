cb2a64983944b6bd9160e583de0bcdace9e8c63f
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-09-28 19:08:09 -0300

    uses pipe to titlecase names

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,5 +1,5 @@
 // Child of the CustomersComponent
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from 'src/app/shared/interfaces';
 
@@ -9,7 +9,21 @@ import { ICustomer } from 'src/app/shared/interfaces';
   styleUrls: ['./customers-list.component.css'],
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomers: ICustomer[] = [];
+  private _customers: ICustomer[] = []; //original value
+  @Input() get customers(): ICustomer[] {
+    //get the value of props passed by CustomerComponent
+    return this._customers; //return its value
+  }
+
+  set customers(value: ICustomer[]) {
+    //assing the value to filteredCustomers and _customers
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
+  filteredCustomers: ICustomer[] = []; //filtered value
   customersOrderTotal!: number;
   currecyCode: string = 'USD';
 
@@ -22,5 +36,9 @@ export class CustomersListComponent implements OnInit {
     this.filteredCustomers.forEach((customer: ICustomer) => {
       this.customersOrderTotal += customer.orderTotal;
     });
+  }
+
+  sort(prop: string) {
+    //A sorter service will handle the sorting
   }
 }
