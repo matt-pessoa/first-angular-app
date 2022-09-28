bd917cc862e2143d2fcc576a56c038abf9f1a8da
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-09-28 16:17:13 -0300

    generate customer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomersComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.css'],
 })
 export class CustomersComponent implements OnInit {
+  title!: string;
+  people!: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.title = 'Customers';
+    this.people = [
+      {
+        id: 1,
+        name: 'john Doe',
+        city: 'Phoenix',
+        orderTotal: 9.99,
+        customerSince: new Date(2014, 7, 10),
+      },
+      {
+        id: 2,
+        name: 'jane doe',
+        city: 'Chandler',
+        orderTotal: 19.99,
+        customerSince: new Date(2017, 2, 22),
+      },
+      {
+        id: 3,
+        name: 'michelle thomas',
+        city: 'Seattle',
+        orderTotal: 99.99,
+        customerSince: new Date(2002, 10, 31),
+      },
+      {
+        id: 4,
+        name: 'jim thomas',
+        city: 'New York',
+        orderTotal: 599.99,
+        customerSince: new Date(2002, 10, 31),
+      },
+    ];
   }
-
 }
