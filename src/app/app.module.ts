d41e0519596bb17da883c7129947df7b2667b0e9
Matt Pessoa <54649950+matt-pessoa@users.noreply.github.com>
2022-10-01 10:39:47 -0300

    Revert "create customers route"
    This reverts commit bfc15a2d9a063e62ceb5b75e84a5fa45a137a03c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,8 @@ import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
-  imports: [
-    BrowserModule,
-    CoreModule,
-    CustomersModule,
-    SharedModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -7,10 +7,9 @@ import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 import { SharedModule } from '../shared/shared.module';
-import { CustomersRoutingModule } from './customers-routing.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, FormsModule, CustomersRoutingModule],
+  imports: [CommonModule, SharedModule, FormsModule],
   declarations: [
     CustomersComponent,
     CustomersListComponent,
