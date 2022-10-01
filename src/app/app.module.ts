cd2eaa7796fec5b8e91d6e923bf473ff007efb4c
Matt Pessoa <54649950+matt-pessoa@users.noreply.github.com>
2022-10-01 11:57:09 -0300

    add app routing module to app module - so any component can use RouterModules features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,15 @@ import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    CustomersModule,
+    SharedModule,
+    AppRoutingModule,
+  ],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
